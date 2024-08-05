fb4d121fd9dfad73aa3745e8bc9f1b40025f729b
Allan Rocha Pereira <ra252694@homer.lab.ic.unicamp.br>
2024-08-05 11:51:12 -0300

    Código Inicial

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{"54.160.9.28"},
+		GroupID:  "a252694",
+		Topic:    "raw-samples",
+		MaxBytes: 10e6, // 1	0MB
+	})
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
+		fmt.Printf("message at topic/partition/offset %v/%v/%v: %s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
+	}
+
+	if err := r.Close(); err != nil {
+		log.Fatal("failed to close reader:", err)
+	}
+}
